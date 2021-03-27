1d4dc7a352681e2ce653cca772c9502db2a91a39
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2021-03-27 10:28:46 +0300

    define a basic boilerplate

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 
 const app = express();
+
+app.engine("ejs", ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -9,19 +12,19 @@ app.set("views", path.join(__dirname, "views"));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.render("home");
+	res.render("home", { title: "Home", scriptRoute: "home" });
 });
 
 app.get("/utilities/random-number", (req, res) => {
-	res.render("utilities/randomNumber");
+	res.render("utilities/randomNumber", { title: "Random Number", scriptRoute: "randomNumber" });
 });
 
 app.get("/utilities/random-color", (req, res) => {
-	res.render("utilities/randomColor");
+	res.render("utilities/randomColor", { title: "Random Color", scriptRoute: "randomColor" });
 });
 
 app.get("/utilities/random-password", (req, res) => {
-	res.render("utilities/randomPassword");
+	res.render("utilities/randomPassword", { title: "Random Password", scriptRoute: "randomPassword" });
 });
 
 app.listen(3000, (req, res) => {
