3ddac04ba71006a6618304cebb663ac7043e385f
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2021-04-16 01:57:21 +0300

    improve validation of random password page

--- public/javascripts/randomPassword.js
+++ public/javascripts/randomPassword.js
@@ -10,9 +10,72 @@ const length = document.querySelector("#length");
 const copyButton = document.querySelector("#copy-button");
 
 button.addEventListener("click", () => {
-	output.innerHTML = random.password({ lowercase: lowercase.checked, uppercase: uppercase.checked, number: number.checked, symbol: symbol.checked, length: length.value });
+	output.innerHTML = random.password({ lowercase: lowercase.checked, uppercase: uppercase.checked, number: number.checked, symbol: symbol.checked, length: parseInt(length.value) });
+	length.value = parseInt(length.value);
 });
 
 copyButton.addEventListener("click", () => {
 	window.navigator.clipboard.writeText(output.innerText);
+});
+
+length.addEventListener("input", () => {
+	if (length.value > 64 || length.value < 4) {
+		button.disabled = true;
+	} else {
+		if (lowercase.checked || uppercase.checked || number.checked || symbol.checked) {
+			button.disabled = false;
+		}
+	}
+});
+
+length.addEventListener("change", () => {
+	if (length.value > 64) {
+		if (lowercase.checked || uppercase.checked || number.checked || symbol.checked) {
+			button.disabled = false;
+		}
+		length.value = 64;
+	} else if (length.value < 4) {
+		if (lowercase.checked || uppercase.checked || number.checked || symbol.checked) {
+			button.disabled = false;
+		}
+		length.value = 4;
+	} else if (length.value <= 64 || length.value >= 4) {
+		if (lowercase.checked || uppercase.checked || number.checked || symbol.checked) {
+			button.disabled = false;
+		}
+	} else {
+		button.disabled = true;
+	}
+});
+
+lowercase.addEventListener("click", () => {
+	if (!lowercase.checked && !uppercase.checked && !number.checked && !symbol.checked && length.value <= 64 && length.value >= 4) {
+		button.disabled = true;
+	} else {
+		button.disabled = false;
+	}
+});
+
+uppercase.addEventListener("click", () => {
+	if (!lowercase.checked && !uppercase.checked && !number.checked && !symbol.checked && length.value <= 64 && length.value >= 4) {
+		button.disabled = true;
+	} else {
+		button.disabled = false;
+	}
+});
+
+number.addEventListener("click", () => {
+	if (!lowercase.checked && !uppercase.checked && !number.checked && !symbol.checked && length.value <= 64 && length.value >= 4) {
+		button.disabled = true;
+	} else {
+		button.disabled = false;
+	}
+});
+
+symbol.addEventListener("click", () => {
+	if (!lowercase.checked && !uppercase.checked && !number.checked && !symbol.checked && length.value <= 64 && length.value >= 4) {
+		button.disabled = true;
+	} else {
+		button.disabled = false;
+	}
 });
