097da9dba57e5fdcced2359f8ad791a334a7e57b
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2021-04-16 22:42:47 +0300

    improve validation of random text page

--- public/javascripts/randomText.js
+++ public/javascripts/randomText.js
@@ -13,24 +13,79 @@ const paragraphButton = document.querySelector("#paragraph");
 const copyButton = document.querySelector("#copy-button");
 let selected;
 
+lengthInput.addEventListener("input", () => {
+	if (syllableButton.checked) {
+		if (lengthInput.value > 4 || lengthInput.value < 2) {
+			button.disabled = true;
+		} else {
+			button.disabled = false;
+		}
+	} else {
+		if (lengthInput.value > 12 || lengthInput.value < 2) {
+			button.disabled = true;
+		} else {
+			button.disabled = false;
+		}
+	}
+});
+
 letterButton.addEventListener("click", () => {
 	unitOutput.innerHTML = "";
+	lengthInput.disabled = true;
+	button.disabled = false;
+	lengthInput.value = "";
 });
 
 syllableButton.addEventListener("click", () => {
 	unitOutput.innerHTML = "letters";
+	lengthInput.disabled = false;
+	if (lengthInput.value > 4 || lengthInput.value < 2) {
+		if (lengthInput.value) {
+			lengthInput.value = 4;
+			button.disabled = false;
+		}
+	} else {
+		button.disabled = false;
+	}
 });
 
 wordButton.addEventListener("click", () => {
 	unitOutput.innerHTML = "letters";
+	lengthInput.disabled = false;
+	if (lengthInput.value > 12 || lengthInput.value < 2) {
+		if (lengthInput.value) {
+			lengthInput.value = 4;
+			button.disabled = false;
+		}
+	} else {
+		button.disabled = false;
+	}
 });
 
 sentenceButton.addEventListener("click", () => {
 	unitOutput.innerHTML = "words";
+	lengthInput.disabled = false;
+	if (lengthInput.value > 12 || lengthInput.value < 2) {
+		if (lengthInput.value) {
+			lengthInput.value = 4;
+			button.disabled = false;
+		}
+	} else {
+		button.disabled = false;
+	}
 });
 
 paragraphButton.addEventListener("click", () => {
 	unitOutput.innerHTML = "sentences";
+	lengthInput.disabled = false;
+	if (lengthInput.value > 12 || lengthInput.value < 2) {
+		if (lengthInput.value) {
+			lengthInput.value = 4;
+			button.disabled = false;
+		}
+	} else {
+		button.disabled = false;
+	}
 });
 
 copyButton.addEventListener("click", () => {
