b236fc523618a53e09fb0d716a076c58258e1c8c
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2021-09-19 11:36:53 +0300

    add undo button to random palette page

--- public/javascripts/randomPalette.js
+++ public/javascripts/randomPalette.js
@@ -4,6 +4,7 @@ const random = require("pengrape");
 
 const buttonGenerate = document.querySelector("#button-generate");
 const buttonCopy = document.querySelector("#button-copy");
+const buttonUndo = document.querySelector("#button-undo");
 const buttonHex = document.querySelector("#button-hex");
 const buttonRgb = document.querySelector("#button-rgb");
 const buttonHsl = document.querySelector("#button-hsl");
@@ -15,7 +16,9 @@ const color4 = document.querySelector("#color-4");
 const color5 = document.querySelector("#color-5");
 
 let index;
+let history = [];
 let color_list = [];
+buttonUndo.disabled = true;
 
 buttonGenerate.addEventListener("click", () => {
 	radioColor.forEach((colorOpt, i) => {
@@ -26,47 +29,89 @@ buttonGenerate.addEventListener("click", () => {
 
 	color_list = random.palette({ format: "all", syntax: "all" });
 
-	color1.style.backgroundColor = color_list.normal[0][0];
-	color1.innerHTML = color_list.normal[index][0];
-	color1.style.color = color_list.list[2][0][2] > 45 ? "black" : "white";
+	history.push(color_list);
 
-	color2.style.backgroundColor = color_list.normal[0][1];
-	color2.innerHTML = color_list.normal[index][1];
-	color2.style.color = color_list.list[2][1][2] > 45 ? "black" : "white";
+	color1.style.backgroundColor = history[history.length - 1].normal[0][0];
+	color1.innerHTML = history[history.length - 1].normal[index][0];
+	color1.style.color = history[history.length - 1].list[2][0][2] > 45 ? "black" : "white";
 
-	color3.style.backgroundColor = color_list.normal[0][2];
-	color3.innerHTML = color_list.normal[index][2];
-	color3.style.color = color_list.list[2][2][2] > 45 ? "black" : "white";
+	color2.style.backgroundColor = history[history.length - 1].normal[0][1];
+	color2.innerHTML = history[history.length - 1].normal[index][1];
+	color2.style.color = history[history.length - 1].list[2][1][2] > 45 ? "black" : "white";
 
-	color4.style.backgroundColor = color_list.normal[0][3];
-	color4.innerHTML = color_list.normal[index][3];
-	color4.style.color = color_list.list[2][3][2] > 45 ? "black" : "white";
+	color3.style.backgroundColor = history[history.length - 1].normal[0][2];
+	color3.innerHTML = history[history.length - 1].normal[index][2];
+	color3.style.color = history[history.length - 1].list[2][2][2] > 45 ? "black" : "white";
 
-	color5.style.backgroundColor = color_list.normal[0][4];
-	color5.innerHTML = color_list.normal[index][4];
-	color5.style.color = color_list.list[2][4][2] > 45 ? "black" : "white";
+	color4.style.backgroundColor = history[history.length - 1].normal[0][3];
+	color4.innerHTML = history[history.length - 1].normal[index][3];
+	color4.style.color = history[history.length - 1].list[2][3][2] > 45 ? "black" : "white";
+
+	color5.style.backgroundColor = history[history.length - 1].normal[0][4];
+	color5.innerHTML = history[history.length - 1].normal[index][4];
+	color5.style.color = history[history.length - 1].list[2][4][2] > 45 ? "black" : "white";
 
 	buttonHex.addEventListener("click", () => {
-		color1.innerHTML = color_list.normal[0][0];
-		color2.innerHTML = color_list.normal[0][1];
-		color3.innerHTML = color_list.normal[0][2];
-		color4.innerHTML = color_list.normal[0][3];
-		color5.innerHTML = color_list.normal[0][4];
+		color1.innerHTML = history[history.length - 1].normal[0][0];
+		color2.innerHTML = history[history.length - 1].normal[0][1];
+		color3.innerHTML = history[history.length - 1].normal[0][2];
+		color4.innerHTML = history[history.length - 1].normal[0][3];
+		color5.innerHTML = history[history.length - 1].normal[0][4];
 	});
 	buttonRgb.addEventListener("click", () => {
-		color1.innerHTML = color_list.normal[1][0];
-		color2.innerHTML = color_list.normal[1][1];
-		color3.innerHTML = color_list.normal[1][2];
-		color4.innerHTML = color_list.normal[1][3];
-		color5.innerHTML = color_list.normal[1][4];
+		color1.innerHTML = history[history.length - 1].normal[1][0];
+		color2.innerHTML = history[history.length - 1].normal[1][1];
+		color3.innerHTML = history[history.length - 1].normal[1][2];
+		color4.innerHTML = history[history.length - 1].normal[1][3];
+		color5.innerHTML = history[history.length - 1].normal[1][4];
 	});
 	buttonHsl.addEventListener("click", () => {
-		color1.innerHTML = color_list.normal[2][0];
-		color2.innerHTML = color_list.normal[2][1];
-		color3.innerHTML = color_list.normal[2][2];
-		color4.innerHTML = color_list.normal[2][3];
-		color5.innerHTML = color_list.normal[2][4];
+		color1.innerHTML = history[history.length - 1].normal[2][0];
+		color2.innerHTML = history[history.length - 1].normal[2][1];
+		color3.innerHTML = history[history.length - 1].normal[2][2];
+		color4.innerHTML = history[history.length - 1].normal[2][3];
+		color5.innerHTML = history[history.length - 1].normal[2][4];
 	});
+
+	if (history.length === 1) {
+		buttonUndo.disabled = true;
+	} else {
+		buttonUndo.disabled = false;
+	}
+});
+
+buttonUndo.addEventListener("click", () => {
+	if (history.length > 1) {
+		history.pop();
+	}
+	radioColor.forEach((colorOpt, i) => {
+		if (colorOpt.checked) {
+			color1.style.backgroundColor = history[history.length - 1].normal[0][0];
+			color1.innerHTML = history[history.length - 1].normal[i][0];
+			color1.style.color = history[history.length - 1].list[2][0][2] > 45 ? "black" : "white";
+
+			color2.style.backgroundColor = history[history.length - 1].normal[0][1];
+			color2.innerHTML = history[history.length - 1].normal[i][1];
+			color2.style.color = history[history.length - 1].list[2][1][2] > 45 ? "black" : "white";
+
+			color3.style.backgroundColor = history[history.length - 1].normal[0][2];
+			color3.innerHTML = history[history.length - 1].normal[i][2];
+			color3.style.color = history[history.length - 1].list[2][2][2] > 45 ? "black" : "white";
+
+			color4.style.backgroundColor = history[history.length - 1].normal[0][3];
+			color4.innerHTML = history[history.length - 1].normal[i][3];
+			color4.style.color = history[history.length - 1].list[2][3][2] > 45 ? "black" : "white";
+
+			color5.style.backgroundColor = history[history.length - 1].normal[0][4];
+			color5.innerHTML = history[history.length - 1].normal[i][4];
+			color5.style.color = history[history.length - 1].list[2][4][2] > 45 ? "black" : "white";
+			index = i;
+		}
+	});
+
+	if (history.length === 1) {
+		buttonUndo.disabled = true;
+	}
 });
 
 buttonCopy.addEventListener("click", () => {
