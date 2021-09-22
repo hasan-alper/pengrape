afb98ed7575e88a296b5dbed572019f6f1c38ac9
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2021-09-22 11:15:03 +0300

    specify metadata like page descriptions

--- app.js
+++ app.js
@@ -19,11 +19,23 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 app.use("/utilities", utilitiesRoute);
 
 app.get("/", (req, res) => {
-	res.render("home", { title: "Home - Pengrape", scriptRoute: "home", layout: "layouts/boilerplate" });
+	res.render("home", {
+		title: "Home - Pengrape",
+		description: "Pengrape is a web service to generate anything random. It has a good number of utilities which has been designed to be lightweight, fast and easy to use.",
+		keywords: "random, generate, number, color, palette, password, spinner, dice, text, date",
+		scriptRoute: "home",
+		layout: "layouts/boilerplate",
+	});
 });
 
 app.get("/docs", (req, res) => {
-	res.render("docs", { title: "Docs - Pengrape", scriptRoute: "docs", layout: "layouts/boilerplate" });
+	res.render("docs", {
+		title: "Docs - Pengrape",
+		description: "Pengrape Full Documentation. Explore the notes and examples to use Pengrape.",
+		keywords: "random, docs, documentation",
+		scriptRoute: "docs",
+		layout: "layouts/boilerplate",
+	});
 });
 
 app.listen(port, (req, res) => {
--- controllers/utilities.js
+++ controllers/utilities.js
@@ -1,31 +1,79 @@
 module.exports.randomNumber = (req, res) => {
-	res.render("utilities/randomNumber", { title: "Random Number - Pengrape", scriptRoute: "randomNumber", layout: "layouts/utilities" });
+	res.render("utilities/randomNumber", {
+		title: "Random Number - Pengrape",
+		description: "Generate random number between the two numbers that you chose.",
+		keywords: "generate, random, number, integer, decimal, odd, even",
+		scriptRoute: "randomNumber",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.randomColor = (req, res) => {
-	res.render("utilities/randomColor", { title: "Random Color - Pengrape", scriptRoute: "randomColor", layout: "layouts/utilities" });
+	res.render("utilities/randomColor", {
+		title: "Random Color - Pengrape",
+		description: "Generate HEX, RGB or HSL color code.",
+		keywords: "generate, random, color, hex, rgb, hsl",
+		scriptRoute: "randomColor",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.randomPalette = (req, res) => {
-	res.render("utilities/randomPalette", { title: "Random Palette - Pengrape", scriptRoute: "randomPalette", layout: "layouts/utilities" });
+	res.render("utilities/randomPalette", {
+		title: "Random Palette - Pengrape",
+		description: "Generate beautiful color combinations.",
+		keywords: "generate, random, palette, colors, harmony",
+		scriptRoute: "randomPalette",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.randomPassword = (req, res) => {
-	res.render("utilities/randomPassword", { title: "Random Password - Pengrape", scriptRoute: "randomPassword", layout: "layouts/utilities" });
+	res.render("utilities/randomPassword", {
+		title: "Random Password - Pengrape",
+		description: "Generate secure passwords based on guidelines that you set.",
+		keywords: "generate, random, password, secure",
+		scriptRoute: "randomPassword",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.spinner = (req, res) => {
-	res.render("utilities/spinner", { title: "Spinner - Pengrape", scriptRoute: "spinner", layout: "layouts/utilities" });
+	res.render("utilities/spinner", {
+		title: "Spinner - Pengrape",
+		description: "Enter names and spin the wheel to pick a random winner.",
+		keywords: "generate, random, spinner, wheel, picker",
+		scriptRoute: "spinner",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.diceRoller = (req, res) => {
-	res.render("utilities/diceRoller", { title: "Dice Roller - Pengrape", scriptRoute: "diceRoller", layout: "layouts/utilities" });
+	res.render("utilities/diceRoller", {
+		title: "Dice Roller - Pengrape",
+		description: "Roll virtual dice.",
+		keywords: "generate, random, dice, die, roll",
+		scriptRoute: "diceRoller",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.randomText = (req, res) => {
-	res.render("utilities/randomText", { title: "Random Text - Pengrape", scriptRoute: "randomText", layout: "layouts/utilities" });
+	res.render("utilities/randomText", {
+		title: "Random Text - Pengrape",
+		description: "Get a random letter, syllable, word, sentence or paragraph.",
+		keywords: "generate, random, text, letter, syllable, word, sentence, paragraph",
+		scriptRoute: "randomText",
+		layout: "layouts/utilities",
+	});
 };
 
 module.exports.randomDate = (req, res) => {
-	res.render("utilities/randomDate", { title: "Random Date - Pengrape", scriptRoute: "randomDate", layout: "layouts/utilities" });
+	res.render("utilities/randomDate", {
+		title: "Random Date - Pengrape",
+		description: "Generate a date between the two dates that you chose.",
+		keywords: "generate, random, date, time",
+		scriptRoute: "randomDate",
+		layout: "layouts/utilities",
+	});
 };
